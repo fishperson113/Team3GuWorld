ff14d35e7a40f707a11fb9b52bb5343f146b5b35
fishperson113 <23520355@gm.uit.edu.vn>
2024-08-19 18:45:45 +0700

    feat (Portal Door):add script and prefab

---
+++ Assets/_Scripts/Units/Door/PortalDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalDoor : MonoBehaviour
+{
+    [SerializeField] private PortalDoor linkedPortal; 
+
+    private bool canTeleport = true; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (linkedPortal != null && canTeleport && collision.CompareTag("Player"))
+        {
+            Teleport(collision.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canTeleport = true;
+        }
+    }
+    private void Teleport(Transform player)
+    {
+        linkedPortal.canTeleport = false;
+
+        // Dịch chuyển người chơi đến vị trí của cổng liên kết
+        player.position = linkedPortal.transform.position;
+    }
+}
