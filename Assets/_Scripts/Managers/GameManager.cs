2ce616225c236850859a3ca6cf98fcb3e553526d
Ama2352 <honguyenminhsang2016@gmail.com>
2024-08-23 21:07:00 +0700

    <feat>: Finished Point + Win Screen

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class GameManager : StaticInstance<GameManager>
 {
+    public GameObject gameFinishedUI;
+    private void Start()
+    {
+        FinishedPoint.endGame += ShowGameFinishedScreen;      
+    }
+    private void OnDisable()
+    {
+        FinishedPoint.endGame -= ShowGameFinishedScreen;
+    }
+    private void ShowGameFinishedScreen()
+    {
+        gameFinishedUI.SetActive(true);
+    }
 
 }
